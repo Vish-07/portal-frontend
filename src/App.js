edab0eb9dde9b85bc320c0b0b48c29c16949d4dd
meetagrawal09 <agrawalmeet91@gmail.com>
2021-06-05 00:12:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import ProjectForm from './ProjectForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  
+  constructor(){
+    super();
+    this.state={
+      display_form:false,
+      profile:[]
+    }
+    this.handleClick=this.handleClick.bind(this)
+    this.fetchData=this.fetchData.bind(this)
+  }
+  
+
+  handleClick(){
+    this.setState({
+      display_form:true
+    })
+  }
+
+
+  componentDidMount(){
+    this.fetchData()
+  }
+
+  fetchData(){
+    fetch('http://127.0.0.1:8000/profile/')
+    .then((response)=>(response.json()))
+    .then((data)=>{
+      this.setState({
+        profile:data
+      })
+      console.log(data)
+    })
+
+    console.log('STATE:',this.state.profile)
+  }
+
+  render(){
+    return (
+      <div className="App">
+
+
+        <div className="card">
+          <div className="card-body">
+            <p>{this.state.profile.stud_name}</p>
+          </div>
+        </div>
+        {
+          this.state.display_form ? 
+          <ProjectForm/>:<button className="btn btn-info" onClick={this.handleClick}>Add Project</button>
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
